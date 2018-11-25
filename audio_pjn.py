40192758b5efa8d6922b797f75f1edd7280de13b
root <root@fotapjn.wkyesjqpov3eja4on3ilvhwfvd.bx.internal.cloudapp.net>
2018-11-25 13:09:06 +0000

    frame lengths

--- audio_pjn.py
+++ audio_pjn.py
@@ -4,12 +4,35 @@ from scipy.fftpack import dct
 
 
 
-sample_rate,signal=wave.read("/home/fotapjn/pjn_tensor/wave6.wav")
+sample_rate,signal=wave.read("/home/fotapjn/pjn_tensor/wave8.wav")
 print(sample_rate)
 print(signal[:1000])
 print(np.shape(signal))
+signal1=signal[0:sample_rate,1]
 
+print("Size of signal1",np.shape(signal1))
 premp_signal=signal[1:]-0.97*signal[:-1]
 premp_signal=np.append(signal[0],premp_signal)
 #print(premp_signal[10000:16000])
 print(np.shape(premp_signal))
+
+
+#Framing 
+
+frame_size=0.025
+stride_size=0.01
+
+
+frame_length= frame_size*sample_rate
+stride_length=stride_size*sample_rate
+
+frame_length=int(round(frame_length))
+stride_lenght=int(round(stride_length))
+signal_length=lenght(premp_signal)
+
+
+
+
+
+
+
